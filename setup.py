a2b6279083b7425b5f5683270e4bb73815b95460
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-02-07 03:17:07 -0800

    Prepare release 0.3.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/JelleZijlstra/aiohttp-sse-client2',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
